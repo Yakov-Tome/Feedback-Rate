0db795f1db9056171cc061c0bab33ff7936eecc3
yakovtome@outlook.com <yakovtome@gmail.com>
2025-02-17 18:17:29 +0200

    Add Feedback & Setting a Proxy

--- Rate-App/src/context/FeedbackContext.jsx
+++ Rate-App/src/context/FeedbackContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -34,9 +33,18 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Add Feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("http://localhost:5000/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   // Edit Feedback
