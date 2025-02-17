2e2f24415028fb5fd575595dfdbcba9fc7e1875c
yakovtome@outlook.com <yakovtome@gmail.com>
2025-02-17 12:19:04 +0200

    Managing Global State

--- Rate-App/src/App.jsx
+++ Rate-App/src/App.jsx
@@ -1,12 +1,17 @@
-import FeedbackItem from "./components/FeedbackItem";
+import { useState } from "react";
+
 import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header text="Rate UI" />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
---
+++ Rate-App/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 7,
+    text: "Great service! The delivery was fast, and the staff was very friendly and patient.",
+  },
+  {
+    id: 2,
+    rating: 6,
+    text: "The shopping experience was good, but I expected faster delivery.",
+  },
+  {
+    id: 3,
+    rating: 5,
+    text: "The product is decent, but the packaging was damaged, and customer service responded late.",
+  },
+];
+
+export default FeedbackData;
