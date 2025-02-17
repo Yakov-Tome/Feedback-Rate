46734dedde2f562c3ebb761ccd4d3c10119176ef
yakovtome@outlook.com <yakovtome@gmail.com>
2025-02-17 13:36:01 +0200

    Real-Time Validation

--- Rate-App/src/components/FeedbackForm.jsx
+++ Rate-App/src/components/FeedbackForm.jsx
@@ -4,8 +4,21 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (event) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
     setText(event.target.value);
   };
 
@@ -21,10 +34,12 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button version="primary" type="submit">
+          <Button isDisabled={btnDisabled} version="primary" type="submit">
             Send
           </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
